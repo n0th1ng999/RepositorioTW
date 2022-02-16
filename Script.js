269375f347ef4426b6c28cf5201f538c0ecfd9b8
n0th1ng999 <tiagogabrielpereira@gmail.com>
2022-02-16 01:52:33 +0000

    ALMOST AT THE END
    SHOP, CART AND PROFILE NEED TO BE COMPLETED

--- Script.js
+++ Script.js
@@ -1,17 +1,47 @@
 const btn = document.getElementById("menu-toggle");
+const sidenav=document.getElementById("sidenavBackground");
 const lines = btn.querySelectorAll(".line");
 const cls = { open: "open", close: "close" };
-let btnClass = cls.open;
+const body = document.getElementsByTagName("body");
+
+let btnClass = cls.close;
+
 
 btn.addEventListener("click", () => {
   if (btn.classList.contains(cls.open)) {
     btn.classList.remove(btnClass);
     btnClass = cls.close;
+    sidenav.classList.toggle("visible");
+    document.body.classList.toggle("NoScroll");
+    
+    
   } else if (btn.classList.contains(cls.close)) {
     btn.classList.remove(btnClass);
     btnClass = cls.open;
+    sidenav.classList.toggle("visible");
+    document.body.classList.toggle("NoScroll");
+    
+   
+    
+    
   }
 
+  
   void btn.offsetWidth;
   btn.classList.add(btnClass);
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
